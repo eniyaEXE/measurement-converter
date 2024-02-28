8591a8b48d6025644ea2fb5b08a6d4860ea53faf
Eniya <161463527+eniyaEXE@users.noreply.github.com>
2024-02-28 12:46:15 +0000

    measurement converter v1
    added comments because I forgot before

--- measurement converter.py
+++ measurement converter.py
@@ -4,22 +4,22 @@ import itertools
 def FahrToCels():
     output = (measure - 32) / 1.8
     roundedOutput = round(output, 2)
-    print("{0}F is {1}C".format(measure, roundedOutput))
+    print("{0}F is {1}C".format(measure, roundedOutput)) # Fahrenheit to Celsius conversion
 
 def CelsToFahr():
     output = (measure * 1.8) + 3
     roundedOutput = round(output, 2)
-    print("{0}C is {1}F".format(measure, roundedOutput))
+    print("{0}C is {1}F".format(measure, roundedOutput)) # Celsius to Fahrenheit conversion
 
 def CentToInch():
     output = measure / 2.54
     roundedOutput = round(output, 2)
-    print("{0}CM is {1}inches".format(measure, roundedOutput))
+    print("{0}CM is {1}inches".format(measure, roundedOutput)) # Centimeters to Inches conversion
 
 def InchToCent():
     output = measure * 2.54
     roundedOutput = round(output, 2)
-    print("{0}Inches is {1}CM".format(measure, roundedOutput))
+    print("{0}Inches is {1}CM".format(measure, roundedOutput)) #Inches to Centimeters conversion
 
 conversionDict = {"A": FahrToCels,
                   "B": CelsToFahr,
@@ -36,4 +36,4 @@ for _ in itertools.count():
         print("please try again.")
         continue
 
-    conversionDict.get(conversion)()
+    conversionDict.get(conversion)() # uses the user input to choose which conversion to use from the dictionary
